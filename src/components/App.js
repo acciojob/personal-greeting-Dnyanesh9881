e3054597fbd3da9bef1faae8d1ef05bb66256688
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-12-21 04:55:22 +0000

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+const [text,setText]=useState("");
+function catchChnage(event){
+  setText(event.target.value);
+}
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <input type="text" onChange={catchChnage} />
+        {
+        text && (
+          <p>Hello {text}!</p>
+        )
+}
     </div>
   )
 }
